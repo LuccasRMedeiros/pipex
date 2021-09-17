3725f281e82b2698b5a4a8651e29b6654a007c4f
Luccas Rocigno <luccasrocigno@hotmail.com>
2021-09-17 20:26:27 -0300

    first test

--- src/pipex.c
+++ src/pipex.c
@@ -6,10 +6,21 @@
 /*   By: lrocigno <lrocigno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 15:36:13 by lrocigno          #+#    #+#             */
-/*   Updated: 2021/09/14 17:40:52 by lrocigno         ###   ########.fr       */
+/*   Updated: 2021/09/17 20:19:45 by lrocigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <libft.h>
 
 /* 
 ** Main function.
 */
+
+int	main(void)
+{
+	char *arg = "ok bro";
+	char *env;
+
+	execve("echo", &arg, &env);
+	return (0);
+}
